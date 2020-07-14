6a474d925af306ef3250da059af2ec90d2a4034d
Rafael Cunha <rafael_ptc@hotmail.com>
2020-07-14 18:50:05 -0400

    Initial Commit

---
+++ Forecast_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Forecast_Test
+//
+//  Created by Rafael Cunha de Oliveira on 2020-07-14.
+//  Copyright © 2020 Rafael Cunha de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
